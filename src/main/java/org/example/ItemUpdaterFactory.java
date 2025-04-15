9aa85df6652e273490a91e68a16b4eb812c4b319
Jorne Reynders <jorne.reyn@gmail.com>
2025-04-15 19:44:25 +0200

    Naming convention

--- src/main/java/org/example/ItemUpdaterFactory.java
+++ src/main/java/org/example/ItemUpdaterFactory.java
@@ -7,25 +7,25 @@ import java.util.Map;
 public class ItemUpdaterFactory {
     //Initialize all the updaters
     private static final ItemUpdater DEFAULT_UPDATER = new DefaultItemUpdater();
-    private static final Map<String, ItemUpdater> matchingUpdaterMap = Map.of(
+    private static final Map<String, ItemUpdater> UPDATER_MAP = Map.of(
             "Aged Brie", new AgedBrieUpdater(),
             "Sulfuras, Hand of Ragnaros", new LegendaryItemUpdater()
     );
-    private static final Map<String, ItemUpdater> prefixUpdaterMap = Map.of(
+    private static final Map<String, ItemUpdater> PREFIX_UPDATER_MAP = Map.of(
             "Backstage passes ", new BackstagePassItemUpdater(),
             "Conjured ", new ConjuredItemUpdater()
     );
 
 
     public static ItemUpdater getUpdaterFor(Item item) {
-        ItemUpdater updater = matchingUpdaterMap.get(item.name);
+        ItemUpdater updater = UPDATER_MAP.get(item.name);
         if (updater != null) {
             return updater;
         }
 
-        for (String prefix : prefixUpdaterMap.keySet()) {
+        for (String prefix : PREFIX_UPDATER_MAP.keySet()) {
             if (item.name.startsWith(prefix)) {
-                return prefixUpdaterMap.get(prefix);
+                return PREFIX_UPDATER_MAP.get(prefix);
             }
         }
         return DEFAULT_UPDATER;
